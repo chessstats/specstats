c9d5dd1b3aeb449666042071b19e44c0571b27ab
chessstats <scalachessgui@gmail.com>
2016-02-05 05:53:33 +0100

    Normalize by stddev

--- R/specstats.R
+++ R/specstats.R
@@ -10,14 +10,16 @@
 spec_stats <- function() {
 require(xtable)
 require(qdapTools)
+require(fidecountries)
 p<-read.table("current/players.txt")
 r<-read.table("current/stats/birthday.txt",header=TRUE)
 p<-p[complete.cases(p$birthday),]
-p[["rsurp"]]=p$rating-lookup(p$birthday,data.frame(r$birthday,r$avgr))
+p[["rsurp"]]=(p$rating-lookup(p$birthday,data.frame(r$birthday,r$avgr)))/lookup(p$birthday,data.frame(r$birthday,r$stddev))
 p[["age"]]=as.integer(2016-p$birthday)
 s<-p[order(-p$rsurp),]
 l<-s[1:1000,]
-l<-l[c("name","country","sex","age","rating","rsurp")]
+l[["country name"]]=fide_to_country_name(l$country)
+l<-l[c("name","country name","sex","age","rating","rsurp")]
 rownames(l)<-NULL
 write.table(l,"current/youngtalents.txt")
 print(xtable(l), type="html", file="current/youngtalents.html")	
